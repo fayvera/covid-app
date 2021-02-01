798d462c292e72cb6143313a3bcbb401a34269f7
Fay Vera <fvera.cavalcante@gmail.com>
2021-02-01 13:59:13 -0500

    adds welcome message and prompt in Cli

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,5 +1,14 @@
 class Cli
 
+    def welcome
+        puts "Welcome!"
+        puts "This app tracks COVID-19 cases in the NYC area."
+        prompt_date
+    end
 
-
+    def prompt_date
+        puts "Please enter date for when you would like COVID-19 case stats:"
+        puts "Date format = mm/dd/yyyy"
+        input = gets.strip
+    end
 end
