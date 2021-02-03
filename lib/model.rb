435965f050d735b694fbba01262278ccee23a387
Fay Vera <fvera.cavalcante@gmail.com>
2021-02-03 15:56:59 -0500

    adds and saves instances and finds by date

--- lib/model.rb
+++ lib/model.rb
@@ -9,13 +9,27 @@ require_relative('../lib/cli')
 
 class Model
 attr_accessor :date_of_interest, :case_count, :hospitalized_count, :death_count
-
+@@all = []
     def initialize(hash)
         hash.each do |key, value|
             self.send("#{key}=", value)
         end
+        save
     end 
 
+    def save
+        @@all << self
+    end
 
- 
+    def self.all
+        @@all
+    end
+    
+    def self.find_by_date(date)
+        @@all.find do |kase|
+            kase.date_of_interest == date
+        end
+    end
 end
+
+# Model.find_by_date(date)
