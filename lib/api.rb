f5020cf49b1b10c1bbf40bf1d9303827348f31cc
Fay Vera <fvera.cavalcante@gmail.com>
2021-02-03 21:45:12 -0500

    adds individual boroughs to cases

--- lib/api.rb
+++ lib/api.rb
@@ -11,7 +11,10 @@ class Api
 
     def cases_by_date
         response = HTTParty.get(@@url)
-        keys_to_extract = "date_of_interest", "case_count", "hospitalized_count", "death_count"
+        keys_to_extract = "date_of_interest", "case_count", "hospitalized_count", "death_count", 
+        "bx_case_count", "bx_hospitalized_count", "bx_death_count", "bk_case_count", "bk_hospitalized_count", "bk_death_count",
+        "mn_case_count", "mn_hospitalized_count", "mn_death_count", "qn_case_count", "qn_hospitalized_count", "qn_death_count",
+        "si_case_count", "si_hospitalized_count", "si_death_count"
         response.each do |hash|
             cases = hash.select{|key, value| keys_to_extract.include? key}.transform_keys!(&:to_sym)
             cases[:date_of_interest] = cases[:date_of_interest].split("T")[0].split("-").rotate(1).join("/")
@@ -21,3 +24,4 @@ class Api
 end
 
 
+Api.new.cases_by_date
