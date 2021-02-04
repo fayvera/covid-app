4661bcef1bfc3e64bcc38bb8f440114204d2a9f0
Fay Vera <fvera.cavalcante@gmail.com>
2021-02-04 12:29:16 -0500

    adds boroughs and proper responses

--- lib/cli.rb
+++ lib/cli.rb
@@ -2,7 +2,6 @@ require 'pry'
 require 'httparty'
 
 require_relative('../lib/api')
-# # require_relative('../lib/cli')
 require_relative('../lib/model')
 class Cli
 
@@ -26,12 +25,16 @@ class Cli
         puts "To exit, type 'Exit'."
         input = gets.strip
             if input == "exit" || input == "Exit"
-                self.exit_program
-            
+                self.exit_program 
             elsif
                 kase = Model.find_by_date(input)
                 self.numbers_by_date(kase)
+            else
+                puts "Invalid option."
+                puts "Please pick again."
+                self.prompt_date
             end
+            
     end
 
     def numbers_by_date(date)
@@ -40,28 +43,26 @@ class Cli
         puts "The number of people hospitalized within 14 days of diagnosis on #{date.date_of_interest} is: #{date.hospitalized_count}."
         puts "The number of deaths with a positive molecular test on #{date.date_of_interest} is: #{date.death_count}."
         puts ""
-        self.prompt_response
+        self.prompt_response(date)
     end
 
-    def prompt_response
+    def prompt_response(date)
         puts "What would you like to do?"
         puts "1. Look at the numbers for a specific borough for this date."
         puts "2. Look into a different date?"
         puts "3. Exit the application, or type 'Exit'."
         input = gets.strip
             if input == "1"
-                self.pick_borough
+                self.pick_borough(date)
             elsif input == "2"
                 self.prompt_date
             elsif input == "3" || input == "Exit" || input == "exit"
                 self.exit_program
-            # if input == "yes" || input ==  "Yes"
-            #     self.prompt_date
-            # else
-            #     # puts "Invalid answer. Please try again"
-            #     self.invalid_date
-            # end
-
+            else
+                puts "Invalid response."
+                puts "Please try again."
+                self.prompt_response
+            end
     end
 
     def invalid_date
@@ -69,13 +70,13 @@ class Cli
         puts "Please try again or type 'Exit' to exit application."
         self.prompt_date
     end
+
     def exit_program
         puts "Thank you visiting and stay safe!"
-        #ends program
         exit
     end
 
-    def pick_borough
+    def pick_borough(date)
         puts "Please pick a borough:"
         puts "1. Bronx"
         puts "2. Brooklyn"
@@ -83,40 +84,76 @@ class Cli
         puts "4. Queens"
         puts "5. Staten Island"
         puts "6. Select a new date"
-        puts "7. Exit application"
+        puts "7. Select a new Borough"
+        puts "8. Exit application"
         puts ""
         input = gets.strip
             if input == "1"
-                self.borough_stats(bx)
+                self.bronx_stats(date)
             elsif input == "2"
-                self.borough_stats(bk)
+                self.brooklyn_stats(date)
             elsif input == "3"
-                self.borough_stats(mn)
+                self.manhattan_stats(date)
             elsif input == "4"
-                self.borough_stats(qn)
+                self.queens_stats(date)
             elsif input == "5"
-                self.borough_stats(si)
+                self.staten_island_stats(date)
             elsif input == "6"
                 self.prompt_date
-            elsif input == "7" || input == "exit" || input == "Exit"
+            elsif input == "7"
+                self.pick_borough
+            elsif input == "8" || input == "exit" || input == "Exit"
                 self.exit_program
             else
                 puts "Invalid response."
                 puts "Please try again."
                 self.pick_borough
             end
-        end
+    end
 
 
-
+    def bronx_stats(date)
+        puts ""
+        puts "The number of positive cases for #{date.date_of_interest} in Bronx is: #{date.bx_case_count}."
+        puts "The number of people hospitalized within 14 days of diagnosis on #{date.date_of_interest} is: #{date.bx_hospitalized_count}."
+        puts "The number of deaths with a positive molecular test on #{date.date_of_interest} is: #{date.bx_death_count}."
+        puts ""
+        self.pick_borough(date)
+    end
+    def brooklyn_stats(date)
+        puts ""
+        puts "The number of positive cases for #{date.date_of_interest} in Brooklyn is: #{date.bk_case_count}."
+        puts "The number of people hospitalized within 14 days of diagnosis on #{date.date_of_interest} is: #{date.bk_hospitalized_count}."
+        puts "The number of deaths with a positive molecular test on #{date.date_of_interest} is: #{date.bk_death_count}."
+        puts ""
+        self.pick_borough(date)
 
     end
-    def borough_stats(borough)
+    def manhattan_stats(date)
         puts ""
-        puts "The number of positive cases for #{date.date_of_interest} is: #{date.case_count}."
-        puts "The number of people hospitalized within 14 days of diagnosis on #{date.date_of_interest} is: #{date.hospitalized_count}."
-        puts "The number of deaths with a positive molecular test on #{date.date_of_interest} is: #{date.death_count}."
+        puts "The number of positive cases for #{date.date_of_interest} in Manhattan is: #{date.mn_case_count}."
+        puts "The number of people hospitalized within 14 days of diagnosis on #{date.date_of_interest} is: #{date.mn_hospitalized_count}."
+        puts "The number of deaths with a positive molecular test on #{date.date_of_interest} is: #{date.mn_death_count}."
         puts ""
+        self.pick_borough(date)
     end
+    def queens_stats(date)
+        puts ""
+        puts "The number of positive cases for #{date.date_of_interest} in Queens is: #{date.qn_case_count}."
+        puts "The number of people hospitalized within 14 days of diagnosis on #{date.date_of_interest} is: #{date.qn_hospitalized_count}."
+        puts "The number of deaths with a positive molecular test on #{date.date_of_interest} is: #{date.qn_death_count}."
+        puts ""
+        self.pick_borough(date)
+    end
+    def staten_island_stats(date)
+        puts ""
+        puts "The number of positive cases for #{date.date_of_interest} in Staten Island is: #{date.si_case_count}."
+        puts "The number of people hospitalized within 14 days of diagnosis on #{date.date_of_interest} is: #{date.si_hospitalized_count}."
+        puts "The number of deaths with a positive molecular test on #{date.date_of_interest} is: #{date.si_death_count}."
+        puts ""
+        self.pick_borough(date)
+    end
+
+
 
 end
--- lib/model.rb
+++ lib/model.rb
@@ -5,10 +5,13 @@ require_relative('../lib/api')
 require_relative('../lib/cli')
 # require_relative('../lib/model')
 
-
-
 class Model
-attr_accessor :date_of_interest, :case_count, :hospitalized_count, :death_count
+attr_accessor :date_of_interest, :case_count, :hospitalized_count, :death_count, 
+:bx_case_count, :bx_hospitalized_count, :bx_death_count, 
+:bk_case_count, :bk_hospitalized_count, :bk_death_count, 
+:mn_case_count, :mn_hospitalized_count, :mn_death_count, 
+:qn_case_count, :qn_hospitalized_count, :qn_death_count, 
+:si_case_count, :si_hospitalized_count, :si_death_count
 @@all = []
     def initialize(hash)
         hash.each do |key, value|
@@ -28,8 +31,10 @@ attr_accessor :date_of_interest, :case_count, :hospitalized_count, :death_count
     def self.find_by_date(date)
         @@all.find do |kase|
             kase.date_of_interest == date
+            # binding.pry
         end
     end
+   
+
 end
 
-# Model.find_by_date(date)
